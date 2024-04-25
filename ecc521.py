38e21065950a8eae42614698d8e37a972ddfa860
Karl Zander <iagmla@nifu.local>
2024-04-25 16:25:31 -0700

    fix

--- ecc521.py
+++ ecc521.py
@@ -23,7 +23,7 @@ def gen_shared_key(priv_key, pub_key, curve_point):
     return (priv_key * pub_key) % curve_point
 
 # P-521 Wierstrass curve W
-curve_point = pow(2, 221) - 1
+curve_point = pow(2, 521) - 1
 
 base_point = gen_base_point(curve_point)
 privA_key = gen_priv_key(base_point)
@@ -34,5 +34,7 @@ pubB_key = gen_public_key(privB_key, base_point, curve_point)
 
 shared_keyA= gen_shared_key(privA_key, pubB_key, curve_point)
 shared_keyB = gen_shared_key(privB_key, pubA_key, curve_point)
+num_bytes = len(number.long_to_bytes(shared_keyA))
 print(shared_keyA, shared_keyB)
+print(num_bytes)
 
